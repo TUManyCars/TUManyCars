3588a57271a812e5ce244ae311c73111f531ae9b
Tobias <tobias.maier@st.oth-regensburg.de>
2024-11-23 13:47:15 +0100

    format restart_runner

--- python/restart_runner_container.py
+++ python/restart_runner_container.py
@@ -1,13 +1,30 @@
 import subprocess
 import time
 
+
 def restart_and_wait_for_service(compose_file_path):
     """Restart a Docker service and wait until it's running using 'docker ps'."""
     try:
-        subprocess.run(['docker-compose', '-f', compose_file_path, 'down', '--volumes', "--remove-orphans"],
-                       check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        subprocess.run(['docker-compose', '-f', compose_file_path, 'up', '-d'],
-                       check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        subprocess.run(
+            [
+                "docker-compose",
+                "-f",
+                compose_file_path,
+                "down",
+                "--volumes",
+                "--remove-orphans",
+            ],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        subprocess.run(
+            ["docker-compose", "-f", compose_file_path, "up", "-d"],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        print("Restarted all containers.")
 
     except subprocess.CalledProcessError as e:
         print(f"An error occurred: {e.stderr.decode()}")
