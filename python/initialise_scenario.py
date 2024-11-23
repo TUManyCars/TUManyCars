888e4f7dab733045bf8a94893212c08132a7da66
Tobias <tobias.maier@st.oth-regensburg.de>
2024-11-23 11:30:01 +0100

    restart cotainers for every new scenario

--- python/initialise_scenario.py
+++ python/initialise_scenario.py
@@ -1,7 +1,12 @@
 import requests
 from scenario_model import Scenario
+from restart_runner_container import restart_and_wait_for_service
+from pathlib import Path
 
 def init_scenario(speed: float, n_cars:int = 5, n_customers:int = 10):
+    compose_file_path = Path(__file__).parent.parent / 'docker-compose.yml'
+    restart_and_wait_for_service(compose_file_path)
+    
     url1 = 'http://localhost:8080/scenario/create'
     params1 = {'numberOfVehicles': n_cars, 'numberOfCustomers': n_customers}
     response = requests.post(url1, params=params1)
