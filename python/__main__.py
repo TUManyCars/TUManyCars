ae812e994b4eae7af12b4b7dc2d47b3bb1eab544
Tobias <tobias.maier@st.oth-regensburg.de>
2024-11-23 10:25:01 +0100

    algorithm V2 -> sent to closest customer

--- python/__main__.py
+++ python/__main__.py
@@ -3,28 +3,41 @@ from scenario_model import Scenario
 from patch_model import VehiclesUpdate, OneVehicleUpdate
 from initialise_scenario import init_scenario
 import time
+from calculate_distance import calculate_distance
 
 scenario, scenario_start_time = init_scenario(0.01)
     
 start_time = time.perf_counter()
 get_url = f'http://localhost:8090/Scenarios/get_scenario/{scenario.id}'
 update_url = f"http://localhost:8090/Scenarios/update_scenario/{scenario.id}"
-currently_serivced = []
+currently_serviced = []
 cars = VehiclesUpdate(vehicles=[])
 while scenario.status != "COMPLETED":
     response = requests.get(get_url)
     scenario = Scenario.parse_obj(response.json())
     for car in scenario.vehicles:
         if car.isAvailable:
+            closest_customer = None
+            min_distance = float('inf')
+            
             for customer in scenario.customers:
-                if customer.awaitingService and customer.id not in currently_serivced:
-                    currently_serivced.append(customer.id)
-                    customer.awaitingService = False
-                    cars.vehicles.append(OneVehicleUpdate(id=car.id, customerId=customer.id))
-                    break
-    print(cars.dict())
-    requests.put(update_url, json=cars.dict())
-    cars = VehiclesUpdate(vehicles=[])
+                if customer.awaitingService and customer.id not in currently_serviced:
+                    distance = calculate_distance(car.coordX, car.coordY, customer.coordX, customer.coordY)
+                    if distance < min_distance:
+                        min_distance = distance
+                        closest_customer = customer
+                        
+            if closest_customer:
+                currently_serviced.append(closest_customer.id)
+                closest_customer.awaitingService = False
+                cars.vehicles.append(OneVehicleUpdate(id=car.id, customerId=closest_customer.id))
+                
+    if cars.vehicles:
+        print(cars.dict())
+        response = requests.put(update_url, json=cars.dict())
+        if response.status_code == 200:
+            print("Update a car.")
+        cars = VehiclesUpdate(vehicles=[])
     time.sleep(2)
     
 print("completed")
