dc7c7b61bd2764ddec3917d395aeec5829b1702f
FloG99 <goeth-florian@freenet.de>
2024-11-24 01:31:40 +0100

    live page parameter fix

--- web/src/app/live/page.tsx
+++ web/src/app/live/page.tsx
@@ -1,15 +1,17 @@
 import { ScenarioLive } from "../_components/ScenarioLive";
 import { HydrateClient } from "~/trpc/server";
 
-export default function LivePage({
+export default async function LivePage({
   searchParams,
 }: {
-  searchParams: { scenarioid: string };
+  searchParams: Record<string, string | string[] | undefined>;
 }) {
+  const scenarioId = searchParams.scenarioid as string;
+
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <ScenarioLive scenarioId={searchParams.scenarioid} />
+        <ScenarioLive scenarioId={scenarioId} />
       </main>
     </HydrateClient>
   );
