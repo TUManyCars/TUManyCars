d0aa115fa521b1a04dfd836812ca75cb3daa3694
Tobias <tobias.maier@st.oth-regensburg.de>
2024-11-26 02:32:05 +0100

    fix everything + mobil header overlap

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -5,11 +5,9 @@ export default function Home() {
   return (
     <HydrateClient>
       <div className="flex flex-col min-h-screen w-screen p-0 m-0">
-        <div className="h-24">
-          <header className="bg-transparent w-full">
-            <h1 className="text-6xl font-bold text-white text-center mt-6">T-Systems Fleet Manager</h1>
-          </header>
-        </div>
+        <header className="bg-transparent w-full py-6 sm:h-24">
+          <h1 className="text-4xl sm:text-6xl font-bold text-white text-center">T-Systems Fleet Manager</h1>
+        </header>
         <div className="flex-1">
           <main className="flex h-full flex-col items-center justify-center text-white">
             <ScenarioStarter />
